35488f18b1c6faa46f3896ccfd1da8bdc85d2273
Cristian Medina <cmedina1996@knights.ucf.edu>
2020-03-29 15:53:49 -0400

    // File Path Restructuring Path 6
    Restructured the folder containing sound effects.
    Had to shorten initial folder, then all of the subsequent folders within it.
    Fixed my melee code so that the sounds referenced are still working.

--- CultyGame/Source/CultyGame/CultyGameCharacter.cpp
+++ CultyGame/Source/CultyGame/CultyGameCharacter.cpp
@@ -61,7 +61,7 @@ ACultyGameCharacter::ACultyGameCharacter()
 	}
 
 	// Load the sound cue object
-	static ConstructorHelpers::FObjectFinder<USoundCue> SwordGestureSoundCueObject(TEXT("SoundCue'/Game/Sounds/RoySwordSwingSoundCue3.RoySwordSwingSoundCue3'"));
+	static ConstructorHelpers::FObjectFinder<USoundCue> SwordGestureSoundCueObject(TEXT("SoundCue'/Game/SFX/Melee_SFX/SwordGestureSoundCue_3.SwordGestureSoundCue_3'"));
 	if (SwordGestureSoundCueObject.Succeeded()) // Null check
 	{
 		SwordGestureSoundCue = SwordGestureSoundCueObject.Object;
